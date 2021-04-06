e13673230d6f4b07d727d6d7fc25acfbb314262f
Siya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-05 20:27:47 -0400

    Update index.js file

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-//traverse the DOM—it's the act of selecting an element from another element.
 
-//The currentTarget property always refers to the element whose event listener triggered the event, opposed to the target property, which returns the element that triggered the event.
-
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(btn => {
-//     btn.addEventListener("click", e => {
-//          //console.log(e.currentTarget.parentElement);
-//         //currentTarget is button element
-//         //parent element of the button is the div class "question-title". so if you click the btn you have access to the quesion-title.we can use traverse to also search for siblings
-
-//         //parentElement x2 displays the two classes
-//         const question = e.currentTarget.parentElement.parentElement;
-//         question.classList.toggle("show-text");
-
-//     });
-
-// });
-
-//OR
-//selects all the questions/for each question in questions,selects button add event listner on btn when u click on it it removes the show test from the question co-class
 
 window.addEventListener("load", () => {});
 
@@ -37,4 +16,4 @@ questions.forEach((question) => {
 		question.classList.toggle("show-text");
 	});
 });
-//if you click on one close the rest
+
